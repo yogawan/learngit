42307a8eeb7bc7a2daf7de3c725b238f5a8f4d62
Yogawan Aditya Pratama <yogawanadityapratama@gmail.com>
2024-08-19 18:45:36 +0700

    menambahkan functional components

--- index.jsx
+++ index.jsx
@@ -1,2 +1,12 @@
 const data = "Hello World";
 console.log(data);
+
+const HomePages = () => {
+    return (
+        <div>
+            <p>Hello World!</p>
+        </div>
+    );
+};
+
+export default HomePages;
