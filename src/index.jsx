94d155e47ddead0c0b1bf4efcdfd9b5a19fdd4bc
Yogawan Aditya Pratama <yogawanadityapratama@gmail.com>
2024-08-19 19:09:11 +0700

    update v2.0

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,10 @@ import FooterAtoms from "./components/FooterAtoms";
 import NavigationAtoms from "./components/NavigationAtoms";
 
 const HomePages = () => {
+    const data = "Hello World";
+    const username = "Yogawan";
+    const password = "123";
+    const dataUser = `${username}, ${password}`;
     return (
         <div>
             <p>{data}</p>
